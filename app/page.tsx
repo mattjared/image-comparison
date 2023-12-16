e4fade17c8778d8384d4442363219c3de27df103
Matt Jared <matt.jared@vercel.com>
2023-12-15 20:32:05 -0600

    Josh Hall

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 export default function Home() {
+  const numTimes = 12;
   return (
     <main className="min-h-screen p-24">
       <h2 className='text-center pb-24 tracking-tight text-2xl font-bold'>Image Comparison</h2>
@@ -8,11 +9,16 @@ export default function Home() {
         <div className="rounded-lg border bg-white text-card-foreground shadow-sm w-full max-w-2xl mx-4 px-4 py-2">
           <h4>Next Image</h4>
           <code></code>
-          <Image src="/chicago.jpg" width="1000" height="500" alt="we chicago-an"/>
+          {Array.from({ length: numTimes }, (_, index) => (
+            <Image key={index} src="/chicago-next-img.jpg" width="1000" height="500" alt="we chicago-an" className="mb-6"/>
+          ))}
         </div>
         <div className="rounded-lg border bg-white text-card-foreground shadow-sm w-full max-w-2xl mx-4 px-4 py-2">
           <h4>Web Image</h4>
-          <img src="/chicago.jpg" width="1000" height="500" alt="we chicago-an" />
+          
+          {Array.from({ length: numTimes }, (_, index) => (
+            <img src="/chicago-img.jpg" width="1000" height="500" alt="we chicago-an" className="mb-6" />
+          ))}
         </div>
       </div>
     </main>
